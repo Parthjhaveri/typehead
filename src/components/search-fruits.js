982ba812ff693e03e1d63af7b216084a3809d3ae
Parthjhaveri <parthjhaveri209@gmail.com>
2019-10-31 23:52:34 -0400

    setup initial handle change logic

--- src/components/search-fruits.js
+++ src/components/search-fruits.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import ReactBootstrap, {Container, Row, Col, FormGroup} from 'react-bootstrap'; 
 
+// IMPORT CHILD
+import SuggestionBox from './suggestion-box';
+
 class SearchFruits extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			'word': 'test'
+			'input_val': '',
+			'fruits': [
+				'Açaí', 'Apple', 'Akee', 'Apricot', 'Avocado', 'Banana', 'Bilberry', 'Blackberry',
+				'Blackcurrant', 'Black sapote', 'Blueberry', 'Boysenberry', 'Buddhas hand', 
+				'Crab apples', 'Currant', 'Cherry', 'Cherimoya', 'Chico fruit', 'Cloudberry', 'Coconut',
+				'Cranberry', 'Cucumber', 'Damson', 'Dates', 'Dragonfruit', 'Pitaya', 'Durian',
+				'Elderberry', 'Feijoa', 'Fig', 'Goji berry', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit',
+				'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul',
+				'Japanese plum', 'Jostaberry', 'Jujube', 'Juniper berry', 'Kiwano', 'Kiwifruit',
+				'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen',
+				'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit',
+				'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Blood orange', 'Clementine',
+				'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon',
+				'Plantain', 'Plum', 'Prune', 'Pineapple', 'Pineberry', 'Plumcot', 'Pomegranate',
+				'Pomelo', 'Purple mangosteen', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan',
+				'Redcurrant', 'Salal', 'Salak', 'Satsuma', 'Soursop', 'Star apple', 'Star fruit',
+				'Strawberry', 'Surinam cherry', 'Tamarillo', 'Tamarind', 'Ugli fruit', 'White currant',
+				'White sapote', 'Yuzu', 'Avocado', 'Bell pepper', 'Chili pepper', 'Corn kernel',
+				'Cucumber', 'Eggplant', 'Olive', 'Pea', 'Pumpkin', 'Squash', 'Tomato', 'Zucchini'
+			]
 		}
+
+		this.handle_input = this.handle_input.bind(this);
 	}
 	render() {
 	    return (
@@ -14,33 +38,42 @@ class SearchFruits extends React.Component {
 				<Container>
 					<h1>Search your Fruit</h1>
 					<hr/>
-					<form>
-						<label htmlFor='search-fruit'>
-							Enter a fruit...
-						</label>
-						<input 
-							type='text'
-							autoCorrect='on'
-							autoFocus='on'
-							aria-required='true'
-							name='search-fruit'
-							className='inp-std'
-							id='search-fruit'
-						/>
-						<input 
-							type='submit'							
-							value='Search'
-							className='ml-3 btn-std'
-							id='find-fruit'
-						/>
-					</form>
+					<div className='std-wrapper'>
+						<form>
+							<label htmlFor='search-fruit'>
+								Enter a fruit...
+							</label>
+							<input 
+								type='text'
+								autoCorrect='on'
+								autoFocus='on'
+								aria-required='true'
+								name='search-fruit'
+								className='inp-std'
+								id='search-fruit'
+								onChange={this.handle_input}
+							/>
+							<input 
+								type='submit'							
+								value='Search'
+								className='ml-3 btn-std'
+								id='find-fruit'
+							/>
+						</form>
+						<SuggestionBox />
+					</div>
 				</Container>
 			</section>			  
 	    )
 	}
 
 	componentDidMount() {
-		console.log(this.state.word);
+		console.log(this.state.fruits);
+	}
+
+	handle_input(event) {
+		this.setState({input_val: event.target.value});
+		console.log(this.state.input_val);
 	}
 }
 
---
+++ src/components/suggestion-box.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactBootstrap, {Container, Row, Col, FormGroup} from 'react-bootstrap'; 
+
+class SuggestionBox extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		}
+	}
+	render() {
+	    return (
+			<div className='suggest-box-wrapper'>
+
+			</div>
+	    )
+	}
+}
+
+export default SuggestionBox;
